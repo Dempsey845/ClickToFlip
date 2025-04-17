524c0babfcfae6d1e0d2e24fde43cb301e883934
Dempsey845 <dempseynewton1@gmail.com>
2025-04-17 21:52:31 +0100

    Basic project setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { config } from "dotenv";
+
+config();
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
