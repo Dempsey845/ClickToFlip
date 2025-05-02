fdb23ffc250f1d085580a51ed81e5d89f437f1cf
Dempsey845 <dempseynewton1@gmail.com>
2025-05-02 15:19:53 +0100

    more updated cookies for production...

--- backend/middleware/session.js
+++ backend/middleware/session.js
@@ -7,6 +7,8 @@ config();
 
 const PgSession = pgSession(session);
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const sessionMiddleware = session({
   store: new PgSession({ pool: db, tableName: "session" }),
   secret: process.env.SESSION_SECRET,
@@ -14,9 +16,10 @@ const sessionMiddleware = session({
   saveUninitialized: false,
   cookie: {
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    httpOnly: true, // Prevents JS access to cookie
+    sameSite: isProduction ? "None" : "Lax", // Allow cross-site only in prod
+    secure: isProduction, // HTTPS-only in production
   },
-  httpOnly: true,
-  sameSite: "None",
 });
 
 export default sessionMiddleware;
