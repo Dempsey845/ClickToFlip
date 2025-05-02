dc0d5d54e467067eae725a50607b582ba1b03849
Dempsey845 <dempseynewton1@gmail.com>
2025-05-02 15:18:47 +0100

    updated cookies for produciton

--- backend/middleware/session.js
+++ backend/middleware/session.js
@@ -16,7 +16,7 @@ const sessionMiddleware = session({
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   },
   httpOnly: true,
-  sameSite: "Strict",
+  sameSite: "None",
 });
 
 export default sessionMiddleware;
